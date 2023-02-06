e80bc55bdadcca06e68da3c77c06b0184688e344
Svaroggod <svaroggod@yandex.ru>
2023-02-06 20:53:36 +0300

    ver 0.0.4

--- routes/trueAnswer.js
+++ routes/trueAnswer.js
@@ -2,10 +2,9 @@ const {Router} = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('trueAns', {
+    res.render('trueAnswer', {
         title: 'Пользователь найден',
     });
 });
-
 
 module.exports = router;    
